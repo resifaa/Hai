74f17b81cc416d812c77f96a0d183ebbae2c94b7
resifaa <94331675+resifaa@users.noreply.github.com>
2021-11-15 09:12:34 +0700

    Add files via upload

---
+++ hai.py
@@ -0,0 +1,9 @@
+Nama = "Resi Frella Angrestu"
+NIM = 212410102070
+Prodi = "Teknologi Informasi"
+Kelompok = 15
+Asal = "Jember"
+Umur = "18 Tahun"
+
+
+print("Perkenalkan nama saya", Nama, "dengan NIM", NIM, "dari prodi", Prodi, "Universitas Jember.", "Saya kelompok", Kelompok, "berasal dari", Asal, "dan berumur", Umur, ":D")
